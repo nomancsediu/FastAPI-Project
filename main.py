17468b327163f7737f1084ac01b559fa4362e7fa
nomancsediu <abdnoman720@gmail.com>
2026-05-01 00:10:35 +0600

    Task model table added

--- main.py
+++ main.py
@@ -1,11 +1,9 @@
 from fastapi import Depends, FastAPI
 from src.utils.db import get_db, Base, engine
+from src.tasks.models import TaskModel
+
+
 
 Base.metadata.create_all(bind=engine)
 
-from sqlalchemy.orm import Session
 app = FastAPI(title="This is my Task Management Application")
-
-@app.get("/db-check")
-def db_check(db: Session = Depends(get_db)):
-    return {"message": "DB connected successfully"}
